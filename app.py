130b8544ea4826a4a53d5dcd054fbe69cf79ebc8
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-29 20:46:20 +0530

    feat: Directory structure generation

---
+++ app.py
@@ -0,0 +1,47 @@
+import json
+import argparse
+from pathlib import Path
+from typing import Dict
+
+# Setup Argument Parser
+parser = argparse.ArgumentParser()
+parser.add_argument("app", help="App to which the SPA should be added")
+parser.add_argument("--add-spa", help="SPA directory/route name", action="store_true")
+
+# Parse the arguments
+args = parser.parse_args()
+
+if args.add_spa:
+	# spa_name = input("SPA name (directory and route): ")
+	# js_framework = input("JS Framwork (vuejs/react): ")
+	# css_library = input("CSS Option (tailwind/bootstrap/vuetify): ")
+	pass
+
+root = Path(".")
+
+# Recursive structure
+with open("directory_schema.json", "r") as f:
+	directory_structure = json.load(f)
+
+
+def generate_directory(dir: Dict, root: Path):
+	print(dir, root)
+
+	folder: Path = root / dir.get("name")
+
+	if not folder.exists():
+		folder.mkdir()
+
+	if dir.get("dirs"):
+		for d in dir.get("dirs"):
+			generate_directory(d, folder)
+
+	if dir.get("files"):
+		for f in dir.get("files"):
+			f_path: Path = root / f.get("name")
+
+			if not f_path.exists():
+				f_path.touch()
+
+
+generate_directory(directory_structure, root)
