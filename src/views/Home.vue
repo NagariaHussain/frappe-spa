37427ab2766c7667ddba9c1528f259305d1ce81f
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-08-07 15:08:46 +0530

    chore: Clean up Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,47 @@
 <template>
-	<div>
-		<main>
-			<div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-				<div class="px-4 py-4 sm:px-0">
-					<div>
-						<p>$resources.ping.loading: {{ $resources.ping.loading }}</p>
-						<p>loading computed: {{ isLoading }}</p>
-						<p>pong computed property: {{ pong }}</p>
-						<p>this.$resources.ping.data: {{ this.$resources.ping.data }}</p>
-					</div>
-					<button
-						class="bg-red-500 rounded px-2 py-2 text-white"
-						@click="$resources.ping.reload()"
-					>
-						Click me
-					</button>
-				</div>
-
-				<div>User is logged in: {{ this.$auth.isLoggedIn }}</div>
-			</div>
-		</main>
-	</div>
+  <div>
+    <main>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="px-4 py-4 sm:px-0">
+          <div>
+            <p>Result: {{ pong }}</p>
+          </div>
+          <button
+            class="bg-red-500 rounded px-2 py-2 text-white"
+            @click="$resources.ping.reload()"
+          >
+            Ping
+          </button>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-
 export default {
-	name: 'Home',
-	inject: ['$auth', '$socket'],
-	data() {
-		return {};
-	},
-	resources: {
-		ping() {
-			return {
-				method: 'frappe.ping',
-			};
-		},
-	},
-	computed: {
-		pong() {
-			if (!this.$resources.ping.data) {
-				return 'No data';
-			}
+  name: "Home",
+  data() {
+    return {};
+  },
+  resources: {
+    ping() {
+      return {
+        method: "frappe.ping",
+      };
+    },
+  },
+  computed: {
+    pong() {
+      if (!this.$resources.ping.data) {
+        return "No data";
+      }
 
-			return this.$resources.ping.data;
-		},
-		isLoading() {
-			return this.$resources.ping.loading;
-		},
-	},
+      return this.$resources.ping.data;
+    },
+    isLoading() {
+      return this.$resources.ping.loading;
+    },
+  },
 };
 </script>
